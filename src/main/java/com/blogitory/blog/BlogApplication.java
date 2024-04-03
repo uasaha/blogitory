1a404a9e203b1fbce279a5706049915310ee4df0
uasaha <uasaha000@naver.com>
2024-04-03 10:11:17 +0900

    [MOD] Add doc

--- src/main/java/com/blogitory/blog/BlogApplication.java
+++ src/main/java/com/blogitory/blog/BlogApplication.java
@@ -3,11 +3,17 @@ package com.blogitory.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Blogitory start.
+ *
+ * @author woonseok
+ * @since 1.0
+ */
 @SpringBootApplication
 public class BlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BlogApplication.class, args);
+  }
 
 }
