0e38d0cebb5d6052ec8c29d5c96d86e731340ee7
uasaha <uasaha000@naver.com>
2024-07-12 15:53:10 +0900

    [Feat] tabler 추가

---
+++ src/main/resources/static/js/lib/theme.js
@@ -0,0 +1,52 @@
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+})((function () { 'use strict';
+
+	let themeStorageKey = "tablerTheme";
+	let defaultTheme = "light";
+	let selectedTheme;
+	let params = new Proxy(new URLSearchParams(window.location.search), {
+	  get: function get(searchParams, prop) {
+	    return searchParams.get(prop);
+	  }
+	});
+	if (!!params.theme) {
+	  localStorage.setItem(themeStorageKey, params.theme);
+	  selectedTheme = params.theme;
+	} else {
+	  var storedTheme = localStorage.getItem(themeStorageKey);
+	  selectedTheme = storedTheme ? storedTheme : defaultTheme;
+	}
+	if (selectedTheme === 'dark') {
+	  document.body.setAttribute("data-bs-theme", selectedTheme);
+	} else {
+	  document.body.removeAttribute("data-bs-theme");
+	}
+
+}));
+
+
+function setTheme(theme) {
+	let themeStorageKey = "tablerTheme";
+	let defaultTheme = "light";
+	let selectedTheme = theme;
+
+	if (localStorage.getItem(themeStorageKey) == null) {
+		localStorage.setItem(themeStorageKey, defaultTheme);
+	}
+
+	// selectedTheme = localStorage.getItem(themeStorageKey);
+
+	if (selectedTheme === "light") {
+		localStorage.setItem(themeStorageKey, "light");
+	} else if (selectedTheme === "dark") {
+		localStorage.setItem(themeStorageKey, "dark");
+	}
+
+	if (selectedTheme === 'dark') {
+		document.body.setAttribute("data-bs-theme", selectedTheme);
+	} else {
+		document.body.removeAttribute("data-bs-theme");
+	}
+}
