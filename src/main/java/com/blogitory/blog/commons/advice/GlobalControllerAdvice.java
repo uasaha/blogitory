026ed590fb7bd673ef292af023720df626d867b0
uasaha <uasaha000@naver.com>
2024-07-30 16:58:12 +0900

    [Feat] 예외처리 추가

---
+++ src/main/java/com/blogitory/blog/commons/advice/GlobalControllerAdvice.java
@@ -0,0 +1,59 @@
+package com.blogitory.blog.commons.advice;
+
+import com.blogitory.blog.member.dto.response.MemberPersistInfoDto;
+import com.blogitory.blog.security.users.UserDetailsImpl;
+import java.util.Objects;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+/**
+ * Global controller advice.
+ *
+ * @author woonseok
+ * @Date 2024-07-18
+ * @since 1.0
+ **/
+@ControllerAdvice
+public class GlobalControllerAdvice {
+  public static final String DEFAULT_ERROR_VIEW = "index/error/error";
+
+  /**
+   * Handle 404.
+   *
+   * @param ex    exception
+   * @param model model
+   * @return error page
+   */
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(value = {NoHandlerFoundException.class})
+  public String handleNoHandlerFoundException(NoHandlerFoundException ex, Model model) {
+    addErrorAttributes(model, HttpStatus.NOT_FOUND.value());
+
+    return DEFAULT_ERROR_VIEW;
+  }
+
+  protected static void addErrorAttributes(Model model, Integer status) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if (authentication.isAuthenticated()
+            && authentication.getDetails() instanceof UserDetailsImpl userDetails) {
+      MemberPersistInfoDto infoDto =
+              new MemberPersistInfoDto(
+                      userDetails.getIdName(), userDetails.getName(), userDetails.getPfp());
+
+      if (Objects.isNull(infoDto.getThumb()) || infoDto.getThumb().isEmpty()) {
+        model.addAttribute("thumbIsNull", true);
+      }
+
+      model.addAttribute("members", infoDto);
+    }
+
+    model.addAttribute("errorCode", status);
+  }
+}
